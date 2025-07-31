1392f801a148429519bfa96759a1e6cae043c72f
NotAlex575 <dapontealessandro89gmail.com>
2025-07-31 14:28:45 +0200

    creato un'arraylist di 5 post con titolo,contenuto,immagine e tags

--- app.js
+++ app.js
@@ -12,4 +12,35 @@ app.listen(port, () =>{
     console.log(`Server in ascolto sulla porta ${port}`);
 })
 
-
+const postList = [
+    {
+        titolo: "titolo1",
+        contenuto: "questo è il primo contenuto che abbiamo nella array list",
+        immagine: "1.jpg",
+        tags: ["fiore", "arancione"]
+    },
+    {
+        titolo: "titolo2",
+        contenuto: "questo è il secondo contenuto che abbiamo nella array list",
+        immagine: "2.jpg",
+        tags: ["montagna", "neve"]
+    },
+    {
+        titolo: "titolo3",
+        contenuto: "questo è il terzo contenuto che abbiamo nella array list",
+        immagine: "3.jpg",
+        tags: ["faro", "mare", "luce"]
+    },
+    {
+        titolo: "titolo4",
+        contenuto: "questo è il quarto contenuto che abbiamo nella array list",
+        immagine: "4.jpg",
+        tags: ["onde", "mare", "luce", "montagna"]
+    },
+    {
+        titolo: "titolo5",
+        contenuto: "questo è il quinto contenuto che abbiamo nella array list",
+        immagine: "5.jpg",
+        tags: ["cuore", "mare", "luce", "arancione"]
+    }
+]
