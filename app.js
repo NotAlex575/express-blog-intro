97e0b6ae64aa028f159a8da70384eafe2937d0ef
NotAlex575 <dapontealessandro89gmail.com>
2025-07-31 14:14:08 +0200

    creata una route / che ritorna un testo semplice con scritto ”Server del mio blog”

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello world")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log("Server del mio blog")
+})
+
+app.listen(port, () =>{
+    console.log(`Server in ascolto sulla porta ${port}`);
+})
